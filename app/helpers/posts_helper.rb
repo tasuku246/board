f226b51ede5203b8a9835da3e5b2fdd4c4cc5812
homma <hitsk26@gmail.com>
2015-03-19 23:41:10 +0900

    h1タグの中のタイトル作成をhelperにした

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,12 @@
 module PostsHelper
 
-
-
   def mention_anchor text,board_thread_id
     ## >>number があったらリンクに変換
     mention_to = text.match(/\A>>[0-9]+/).to_s.sub(">>","")
     text.sub(/\A>>[0-9]+/,"<a href=\"/board_threads/#{board_thread_id}/posts/#{mention_to}\">#{mention_to}</a>")
+  end
+
+  def make_title board_thread
+    board_thread.title + ' 投稿数(' + board_thread.posts.count.to_s + ')'
   end
 end
